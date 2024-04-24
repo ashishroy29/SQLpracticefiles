538e3fdcc1f7b6d87824687e80f74b6a529466e9
Ashish Roy <84368577+ashishroy29@users.noreply.github.com>
2024-04-24 12:11:33 -0400

    Add files via upload

---
+++ SQL practice.sql
@@ -0,0 +1,19 @@
+show databases;
+
+CREATE DATABASE pet_shop;
+
+CREATE DATABASE SlimeStore;
+
+CREATE DATABASE Chicken_coop;
+
+DROP DATABASE SlimeStore;
+
+DROP DATABASE pet_shop;
+
+DROP DATABASE chicken_coop;
+
+show databases;
+
+USE chicken_coop;
+
+SELECT DATABASE();
